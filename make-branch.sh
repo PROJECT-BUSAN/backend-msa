c28659d9266a988d31662dab8f1d6b83c29ec96f
hyun98 <hyun0404woo@gmail.com>
2022-01-30 07:24:31 +0900

    init

---
+++ make-branch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Enter new branch name: "
+read newbranch
+
+git checkout --orphan $newbranch
+git rm --cached -r .
+
+find . \! \( -type d -name .git -prune \) \! -name make-branch.sh -exec rm -rf {} \;
+
+echo "Delete unnecessary function files ..."
+
+commitmsg="init"
+
+git add .
+git commit -m $commitmsg
+git push origin $newbranch
+
+git switch dev
+git merge $newbranch --allow-unrelated-histories
+git push origin dev
+
+git switch $newbranch
+
+rm make-branch.sh
