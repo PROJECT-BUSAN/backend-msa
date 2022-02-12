1e5afd759e0ff53048332d06d58c878fb3e61213
hyun98 <hyun0404woo@gmail.com>
2022-02-12 19:54:18 +0900

    bug fix

--- apigateway-service/api/middleware.py
+++ apigateway-service/api/middleware.py
@@ -1,6 +1,3 @@
-import requests
-
-from django.middleware.security import SecurityMiddleware
 from django.conf import settings
 
 
@@ -9,20 +6,12 @@ class APIGateway:
         self.get_response = get_response
     
     def __call__(self, request):
-        # if hasattr(self, 'process_request'):
-        #     request = self.process_request(request)
-            
-        flag = False
-        for func in settings.APIGATEWAY_INNER_FUNCTION:
-            if request.path.startswith(func):
-                flag = True
-                break
-        
-        if not flag:
-            request.path_info = "/api/gateway"
+        if hasattr(self, 'process_request'):
+            request = self.process_request(request)
         
         print("path info : ", request.path_info)
         print("origin path : ", request.path)
+        
         # call view
         response = self.get_response(request)
 
