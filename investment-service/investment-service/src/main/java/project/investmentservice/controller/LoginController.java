91ce1d2dbd1fea2afb20f7836a820b06400353ac
jojaeng2 <ds4ouj@naver.com>
2022-02-13 14:12:42 +0900

    create session example

---
+++ investment-service/investment-service/src/main/java/project/investmentservice/controller/LoginController.java
@@ -0,0 +1,11 @@
+package project.investmentservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class LoginController {
+}
