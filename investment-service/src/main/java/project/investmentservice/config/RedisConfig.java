70e12efb0120a689dcf256b6f1f7e62122fa0f3a
jojaeng2 <ds4ouj@naver.com>
2022-02-17 22:02:53 +0900

    Create chatting

--- investment-service/src/main/java/project/investmentservice/config/RedisConfig.java
+++ investment-service/src/main/java/project/investmentservice/config/RedisConfig.java
@@ -1,33 +1,35 @@
 package project.investmentservice.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Value("${spring.redis.host}")
-    private String host;
 
-    @Value("${spring.redis.port}")
-    private int port;
-
+    /**
+     * redis pub/sub 메시지를 처리하는 listener 설정
+     */
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        this.port = 6379;
-        return new LettuceConnectionFactory(host, port);
+    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
     }
 
+    /**
+     * 어플리케이션에서 사용할 redisTemplate 설정
+     */
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
         return redisTemplate;
     }
 }
---
+++ investment-service/src/main/java/project/investmentservice/domain/ChatRoom.java
@@ -0,0 +1,23 @@
+package project.investmentservice.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
+    private String roomId;
+    private String name;
+
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
+    }
+}
