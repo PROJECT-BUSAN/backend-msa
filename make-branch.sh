f1696e88665e19eef5a1bebe2fccf8e9bdc3815e
Henu <hyun0404woo@gmail.com>
2022-02-20 20:36:38 +0900

    init

---
+++ make-branch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Enter new branch name: "
+read newbranch
+
+git checkout --orphan $newbranch
+git rm --cached -r .
+
+find . \! \( -type d -name .git -prune \) \! -name make-branch.sh -exec rm -rf {} \;
+
+echo " ========================================="
+echo "| Deleting unnecessary function files ... |"
+echo " ========================================="
+commitmsg="init"
+
+git add .
+git commit -m $commitmsg
+git push origin $newbranch
+
+git switch dev
+git pull origin dev
+git merge $newbranch --allow-unrelated-histories
+git push origin dev
+
+git switch $newbranch
+chmod +x ./make-branch.sh
+echo " =========================================="
+echo "| Start developing on your new branch now! |"
+echo " =========================================="
