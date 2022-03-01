de2737ddb4cf21e32eaadff89cb3ef2470752eb1
hyun98 <hyun0404woo@gmail.com>
2022-03-01 21:22:08 +0900

    test

--- image-build.sh
+++ image-build.sh
@@ -2,8 +2,8 @@
 
 docker build -t apigateway  ./apigateway-service/
 
-# docker build -t profile-service  ./profile-service/profile-service/
+docker build -t profile-service  ./profile-service/profile-service/
 
-docker build -t crawling  ./crawling-service/
+# docker build -t crawling  ./crawling-service/
 
-docker build -t investment-service ./investment-service/
+# docker build -t investment-service ./investment-service/
